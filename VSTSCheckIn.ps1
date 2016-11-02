f6a87c5866dc7f397009061df4081b8fd7d7554d
Skub <haytrun@gmail.com>
2016-11-02 23:31:09 +1100

    added assembly types

--- VSTSCheckIn.ps1
+++ VSTSCheckIn.ps1
@@ -1,1 +1,7 @@
-df
+
+Add-Type -AssemblyName "System.Net"
+Add-Type -Path "$($agentWorkerModulesPath)\Microsoft.TeamFoundation.Client.dll"
+Add-Type -Path "$($agentWorkerModulesPath)\Microsoft.TeamFoundation.Common.dll"
+Add-Type -Path "$($agentWorkerModulesPath)\Microsoft.TeamFoundation.VersionControl.Client.dll"
+Add-Type -Path "$($agentWorkerModulesPath)\Microsoft.TeamFoundation.WorkItemTracking.Client.dll"
+Add-Type -Path "$($agentWorkerModulesPath)\Microsoft.TeamFoundation.Diff.dll"
